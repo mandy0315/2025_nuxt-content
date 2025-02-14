1b9f880c77fba5452b4feaf1ad0213b1d1150b74
mandy <mandy3152618@gmail.com>
2025-02-14 11:09:44 +0800

    add: 日期 Icon 與調整間距

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -5,20 +5,24 @@ const { data: posts } = await useAsyncData(route.path, () => {
 })
 </script>
 <template>
-  <h1 class="text-4xl text-center font-bold py-4 capitalize">blog</h1>
+  <h1 class="text-4xl text-center font-bold py-4 capitalize">文章</h1>
 
   <div class="grid grid-cols-3 gap-4">
 
-    <div v-for="post in posts" class="border bg-white rounded overflow-hidden col-span-1">
+    <div v-for="post in posts" class="border bg-gray-50 rounded overflow-hidden col-span-1">
       <NuxtLink :to="post.path">
         <div class="w-full h-0 pb-[56.25%] relative">
           <img class="h-full object-center object-cover w-full absolute" :src="post.image" />
         </div>
       </NuxtLink>
 
-      <p v-date-format="post.date" class="inline-block text-gray-500"></p>
+      <p class="text-gray-500 text-sm p-2">
+        <Icon class="align-middle mr-1" name="solar:calendar-linear" />
+        <span v-date-format="post.date" class="align-middle"></span>
+      </p>
 
-      <NuxtLink :to="post.path">
+
+      <NuxtLink class="pb-2 px-2 block" :to="post.path">
         <p class="text-2xl"> {{ post.title }}</p>
         <p class="text-gray-500">{{ post.description }}</p>
       </NuxtLink>
