88f761ffabd5cc29d5b67916f3dc6690655a3fb6
mandy <mandy3152618@gmail.com>
2025-02-17 17:05:09 +0800

    add: UPostCard 元件

---
+++ components/U/PostCard.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  path: string,
+  image: string,
+  date: string,
+  title: string,
+  description: string,
+}>()
+</script>
+<template>
+  <div class="bg-gray-100 dark:bg-gray-100/10 border c-border-gray shadow-lg rounded overflow-hidden col-span-1">
+    <NuxtLink :to="path">
+      <div class="w-full h-0 pb-[56.25%] relative">
+        <img class="h-full object-center object-cover w-full absolute" :src="image" />
+      </div>
+    </NuxtLink>
+
+    <UDate :date class="p-2" />
+
+    <NuxtLink class="pb-2 px-2 block" :to="path">
+      <p class="text-2xl"> {{ title }}</p>
+      <p>{{ description }}</p>
+    </NuxtLink>
+  </div>
+</template>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -8,22 +8,6 @@ const { data: posts } = await useAsyncData(route.path, () => {
   <h1 class="text-4xl text-center font-bold py-4 capitalize">文章</h1>
 
   <div class="grid grid-cols-3 gap-4">
-
-    <div v-for="post in posts"
-      class="bg-gray-100 dark:bg-gray-100/10 border c-border-gray shadow-lg rounded overflow-hidden col-span-1">
-      <NuxtLink :to="post.path">
-        <div class="w-full h-0 pb-[56.25%] relative">
-          <img class="h-full object-center object-cover w-full absolute" :src="post.image" />
-        </div>
-      </NuxtLink>
-
-      <UDate :date="post.date" class="p-2" />
-
-      <NuxtLink class="pb-2 px-2 block" :to="post.path">
-        <p class="text-2xl"> {{ post.title }}</p>
-        <p>{{ post.description }}</p>
-      </NuxtLink>
-
-    </div>
+    <UPostCard v-for="post in posts" v-bind="post" :key="post.title" />
   </div>
 </template>
