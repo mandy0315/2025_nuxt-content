2f4f6d72025c21f842597702716d3ca7de8d06dd
mandy <mandy3152618@gmail.com>
2025-02-17 13:24:24 +0800

    add: UDate 元件

---
+++ components/U/Date.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  date: Date | null
+}>()
+</script>
+<template>
+  <p class="c-text-gray text-sm p-2">
+    <Icon class="align-middle mr-1" name="solar:calendar-linear" />
+    <span v-if="date" v-date-format="date" class="align-middle"></span>
+  </p>
+</template>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -9,22 +9,18 @@ const { data: posts } = await useAsyncData(route.path, () => {
 
   <div class="grid grid-cols-3 gap-4">
 
-    <div v-for="post in posts" class="border bg-gray-50 rounded overflow-hidden col-span-1">
+    <div v-for="post in posts" class="bg-gray-100 dark:bg-opacity-10 shadow-lg rounded overflow-hidden col-span-1">
       <NuxtLink :to="post.path">
         <div class="w-full h-0 pb-[56.25%] relative">
           <img class="h-full object-center object-cover w-full absolute" :src="post.image" />
         </div>
       </NuxtLink>
 
-      <p class="text-gray-500 text-sm p-2">
-        <Icon class="align-middle mr-1" name="solar:calendar-linear" />
-        <span v-date-format="post.date" class="align-middle"></span>
-      </p>
-
+      <UDate :date="post.date" />
 
       <NuxtLink class="pb-2 px-2 block" :to="post.path">
         <p class="text-2xl"> {{ post.title }}</p>
-        <p class="text-gray-500">{{ post.description }}</p>
+        <p>{{ post.description }}</p>
       </NuxtLink>
 
     </div>
