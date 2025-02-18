3b7c97ed80b656a5e9691c08c279bf407a958c15
mandy <mandy3152618@gmail.com>
2025-02-18 14:13:35 +0800

    add: 客戶端渲染元件

--- components/ColorModelIcon.vue
+++ components/ColorModelIcon.vue
@@ -7,9 +7,11 @@ const toggleTheme = () => {
 <template>
 
   <div>
-    <button @click="toggleTheme" class="w-8 h-8 hover:bg-gray-100/20 rounded">
-      <Icon v-if="colorMode.value === 'light'" size="1.5rem" name="solar:sun-outline" class="align-middle" />
-      <Icon v-else size="1.3rem" name="solar:moon-outline" class="align-middle" />
-    </button>
+    <ClientOnly>
+      <button @click="toggleTheme" class="w-8 h-8 hover:bg-gray-100/20 rounded">
+        <Icon v-if="colorMode.value === 'light'" size="1.5rem" name="solar:sun-outline" class="align-middle" />
+        <Icon v-else size="1.3rem" name="solar:moon-outline" class="align-middle" />
+      </button>
+    </ClientOnly>
   </div>
 </template>
