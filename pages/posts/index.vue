8d0ee8b402cb56f1822d27d5aaa0a87bccd1e7a9
mandy <mandy3152618@gmail.com>
2025-02-18 14:26:01 +0800

    fix: 文章列表樣式與設定

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
 const route = useRoute();
 const { data: posts } = await useAsyncData(route.path, () => {
-  return queryCollection('posts').order('date', 'DESC').select('title', 'path', 'tags', 'image', 'description', 'date').all()
+  return queryCollection('posts').order('date', 'DESC').select('title', 'path', 'categorys', 'image', 'description', 'date').all()
 })
 </script>
 <template>
-  <h1 class="text-4xl text-center font-bold py-4 capitalize">文章</h1>
+  <div>
+    <UPageTitle>
+      文章
+      <template #directions>
+        目前有
+        <span class="text-c-main-blue font-medium">{{ posts?.length || 0 }}</span>
+        篇文章，紀錄著我生活大小事，歡迎閱讀！
+      </template>
+    </UPageTitle>
 
-  <div class="grid grid-cols-3 gap-4">
-    <UPostCard v-for="post in posts" v-bind="post" :key="post.title" />
+    <div class="grid grid-cols-3 gap-4">
+      <UPostCard v-for="post in posts" v-bind="post" :key="post.title" />
+    </div>
   </div>
 </template>
