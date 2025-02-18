1aa51349f7f016950f7fe12602ff8a57ab865318
mandy <mandy3152618@gmail.com>
2025-02-18 14:21:07 +0800

    add: 頁面標題樣式元件

---
+++ components/U/PageTitle.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div class="pt-6 pb-10">
+    <h1 class="text-4xl text-center font-bold pb-4">
+      <slot />
+    </h1>
+    <p class="text-lg text-center c-text-gray">
+      <slot name="directions" />
+    </p>
+  </div>
+</template>
