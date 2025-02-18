95409e9994de9aeba5b198bbbb0d9768af82920c
mandy <mandy3152618@gmail.com>
2025-02-18 14:24:56 +0800

    add: 分類文章列表

---
+++ pages/categorys/[category].vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const route = useRoute();
+
+// 分類文章列表
+const { data: posts } = await useAsyncData(route.path, async () => {
+  const category = decodeURIComponent(`${route.params.category}`);
+
+  const allPost = await queryCollection('posts')
+    .order('date', 'DESC')
+    .select('title', 'path', 'categorys', 'image', 'description', 'date')
+    .all();
+
+  return allPost.filter(post => post.categorys.includes(category)) || [];
+})
+
+</script>
+<template>
+  <div>
+    <UPageTitle>
+      文章分類
+      <template #directions>
+        分類：
+        <span class="text-c-main-blue font-medium">"{{ route.params.category }}"</span>，
+        目前有
+        <span class="text-c-main-blue font-medium">"{{ posts?.length || 0 }}"</span>
+        篇文章，歡迎閱讀！
+      </template>
+    </UPageTitle>
+
+    <div class="grid grid-cols-3 gap-4">
+      <UPostCard v-for="post in posts" v-bind="post" :key="post.title" />
+    </div>
+  </div>
+</template>
