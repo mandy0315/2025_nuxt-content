2152dca45e0f0d3bbe5dd7d94ad87bab4dd7bc4d
mandy <mandy3152618@gmail.com>
2025-02-19 09:56:50 +0800

    style: 全站內容的最大寬度

--- app.vue
+++ app.vue
@@ -7,7 +7,7 @@ const colorMode = useColorMode();
   <Body :class="colorMode.value">
     <AppHeader />
 
-    <main class="min-h-[calc(100vh-theme(spacing.c-header-height))]">
+    <main class="max-w-[1200px] px-4 mx-auto min-h-[calc(100vh-theme(spacing.c-header-height))]">
       <NuxtPage />
     </main>
 
