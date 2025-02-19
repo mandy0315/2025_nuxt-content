79c597a426962349b8df59a72e8014a9ce820fec
mandy <mandy3152618@gmail.com>
2025-02-19 09:59:11 +0800

    add: 設定 ContentWithSidebar 的 layout

---
+++ layouts/ContentWithSidebar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div class="grid grid-cols-10 gap-x-4">
+    <div class="col-span-8 pb-10">
+      <slot />
+    </div>
+    <div class="col-span-2">
+      <!-- 右側目錄 -->
+      <aside class="sticky top-c-header-height">
+        <slot name="right-side" />
+      </aside>
+    </div>
+  </div>
+</template>
