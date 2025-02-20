2722a22b2ad14ad951a43b97d3688284433957d0
mandy <mandy3152618@gmail.com>
2025-02-20 16:06:12 +0800

    style: 新增樣式

--- components/ColorModelIcon.vue
+++ components/ColorModelIcon.vue
@@ -8,7 +8,7 @@ const toggleTheme = () => {
 
   <div>
     <ClientOnly>
-      <button @click="toggleTheme" class="w-8 h-8 hover:bg-gray-100/20 rounded">
+      <button @click="toggleTheme" class="w-8 h-8 hover:bg-gray-100 dark:hover:bg-gray-100/20 rounded">
         <Icon v-if="colorMode.value === 'light'" size="1.5rem" name="solar:sun-outline" class="align-middle" />
         <Icon v-else size="1.3rem" name="solar:moon-outline" class="align-middle" />
       </button>
