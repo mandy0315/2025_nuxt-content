2d8bc9bec126f278715276b4c7806b3b78c50a3f
mandy <mandy3152618@gmail.com>
2025-02-20 16:12:35 +0800

    add: toggleSidebar 方法與樣式

--- layouts/ContentWithSidebar.vue
+++ layouts/ContentWithSidebar.vue
@@ -1,14 +1,37 @@
 <script setup lang="ts">
+const cookie = useCookie('isShowSidebar', {
+  maxAge: 60 * 60 * 24 * 7, // 設定 7 天
+  path: '/'
+});
+const isShowSidebar = ref(true);
+const toggleSidebar = () => {
+  isShowSidebar.value = !isShowSidebar.value;
+  cookie.value = String(isShowSidebar.value);
+}
+onMounted(() => {
+  isShowSidebar.value = Boolean(cookie.value);
+});
+
 
 </script>
 <template>
-  <div class="grid grid-cols-10 gap-x-4">
-    <div class="col-span-8 pb-10">
-      <slot name="content" />
+  <div class="flex">
+    <div class="pb-10 relative w-full">
+      <button class="sticky top-c-header-height left-full pt-2" @click="toggleSidebar">
+        <div class="rounded-l c-border-btn border-r-transparent">
+          <Icon v-if="isShowSidebar" name="solar:double-alt-arrow-left-line-duotone" size="1.5rem"
+            class="align-middle" />
+          <Icon v-else name="solar:double-alt-arrow-right-line-duotone" size="1.5rem" class="align-middle" />
+        </div>
+      </button>
+      <div class="pr-4">
+        <slot name="content" />
+      </div>
     </div>
-    <div class="col-span-2">
+    <div class="transition-all ease-in delay-150" :class="isShowSidebar ? 'w-60' : 'w-0'">
       <!-- 右側目錄 -->
-      <aside class="sticky top-c-header-height">
+      <aside class="sticky top-c-header-height transition-all pt-10 px-4"
+        :class="isShowSidebar ? 'opacity-100 delay-300' : 'opacity-0'">
         <slot name="right-side" />
       </aside>
     </div>
