5419fcdccb5bb0e269c2b352830b063aa5320362
mandy <mandy3152618@gmail.com>
2025-02-21 15:13:41 +0800

    delete: 刪除舊 ui 元件

--- components/U/RightSideTitle.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <p class="font-medium pb-2 text-lg">
-    <slot />
-  </p>
-</template>
--- components/U/Tag.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts">
-</script>
-<template>
-  <div class="px-3 py-1 rounded-2xl c-border-btn hover:text-c-main-blue">
-    <slot />
-  </div>
-</template>
