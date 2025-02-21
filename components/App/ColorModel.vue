ed6e1e7f4278ecd0175864fe54da8d51576276d8
mandy <mandy3152618@gmail.com>
2025-02-21 14:26:05 +0800

    move: colorModel 元件移到 APP 資料夾

---
+++ components/App/ColorModel.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+const toggleTheme = () => {
+  colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark';
+}
+</script>
+<template>
+
+  <div>
+    <ClientOnly>
+      <button @click="toggleTheme" class="w-8 h-8 hover:bg-gray-100 dark:hover:bg-gray-100/20 rounded">
+        <Icon v-if="colorMode.value === 'light'" size="1.5rem" name="solar:sun-outline" class="align-middle" />
+        <Icon v-else size="1.3rem" name="solar:moon-outline" class="align-middle" />
+      </button>
+    </ClientOnly>
+  </div>
+</template>
--- components/App/Header.vue
+++ components/App/Header.vue
@@ -2,6 +2,6 @@
   <header
     class="bg-[var(--bg-color)]/80 backdrop-blur-sm h-c-header-height flex border-b c-border-gray w-full sticky top-0 justify-between z-50">
     <p>header</p>
-    <ColorModelIcon />
+    <AppColorModel />
   </header>
 </template>
