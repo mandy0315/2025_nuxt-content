6e34b34765315141e0eba2ebc40c8a272987ac4b
mandy <mandy3152618@gmail.com>
2025-04-21 17:41:55 +0800

    fix: 載入分類列表和去分類頁面與文章列表方法

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,47 +1,51 @@
 <script setup lang="ts">
-const route = useRoute();
+
+
 const { getCategories, goToCategoryPage } = useCategory();
+const categories = await getCategories(10);
+const currentPage = ref(1);
+const currentSort = ref('desc');
+const { updatePosts, posts } = usePost();
+updatePosts(currentPage.value, currentSort.value);
 
-const { data: posts } = await useAsyncData(route.path, () => {
-  return queryCollection('posts').order('date', 'DESC').select('title', 'path', 'categorys', 'image', 'description', 'date').all()
-})
+watch([currentPage, currentSort], ([page, sort]) => {
+  updatePosts(page, sort);
+});
 
-const categorys = await getCategories(10);
-
-
-// const testPost =
-// {
-//   title: 'test',
-//   path: '/posts/test',
-//   categorys: ['test'],
-//   image: '/images/posts/twioon.jpg',
-//   description: 'test',
-//   date: '2021-10-10'
-// };
-// // 做十筆 testPost
-// const testPosts = Array.from({ length: 10 }, () => testPost);
 </script>
 <template>
   <NuxtLayout name="content-with-sidebar">
     <template #content>
-      <BaseListTitle>
-        文章
-        <template #directions>
-          目前有
-          <span class="text-c-main-blue font-medium">"{{ posts?.length || 0 }}"</span>
-          篇文章，紀錄著我生活大小事，歡迎閱讀！
-        </template>
-      </BaseListTitle>
+      <div v-if="posts">
+        <BaseListTitle>
+          文章
+          <template #directions>
+            目前有
+            <span class="text-c-main-blue font-medium">"{{ posts.totalList || 0 }}"</span>
+            篇文章，紀錄著我生活大小事，歡迎閱讀！
+          </template>
+        </BaseListTitle>
 
-      <div class="grid grid-cols-3 gap-4">
-        <BasePostCard v-for="post in posts" v-bind="post" :key="post.title" class="col-span-1" />
+        <div class="pb-4 ml-auto text-right">
+          <p class="c-text-gray inline-block">文章排序：</p>
+          <select v-model="currentSort" class="c-border-btn rounded py-1 px-2">
+            <option value="desc">新到舊</option>
+            <option value="asc">舊到新</option>
+          </select>
+        </div>
+
+        <div class="grid grid-cols-3 gap-4">
+          <BasePostCard v-for="post in posts.list" v-bind="post" :key="post.title" class="col-span-1" />
+        </div>
+
+        <BasePagination v-model:current-page="currentPage" :totalPage="posts.totalPage" />
+
       </div>
-
     </template>
     <template #right-side>
       <BaseSidebarTitle>分類</BaseSidebarTitle>
       <div class="flex flex-wrap gap-2">
-        <button class="cursor-pointer" v-for="category in categorys" :key="category"
+        <button class="cursor-pointer" v-for="category in categories" :key="category"
           @click="goToCategoryPage(category)">
           <BaseTag>{{ category }}</BaseTag>
         </button>
