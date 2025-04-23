87931a81859504516619c9541782acf4dd5e944d
mandy <mandy3152618@gmail.com>
2025-04-23 18:33:52 +0800

    fix: 新增 onMounted

--- pages/posts/[slug].vue
+++ pages/posts/[slug].vue
@@ -86,9 +86,12 @@ const initRightSide = async () => {
     }),
   });
 }
-nextTick(() => {
-  initRightSide();
+onMounted(() => {
+  nextTick(() => {
+    initRightSide();
+  });
 })
+
 </script>
 
 <template>
