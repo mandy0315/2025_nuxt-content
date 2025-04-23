a9fe9d2fe06e74df8b5344b2ab6eee11804a1cd5
mandy <mandy3152618@gmail.com>
2025-04-23 19:14:23 +0800

    feat: 單行 code 元件與文件

---
+++ components/content/ProseCode.vue
@@ -0,0 +1,17 @@
+<template>
+  <code class="bg-gray-200/20 px-2 py-1 rounded border border-gray-300 dark:bg-black/50">
+    <slot />
+  </code>
+</template>
+
+<style lang="scss">
+.prose {
+  :where(code):not(:where([class~="not-prose"], [class~="not-prose"] *))::before {
+    content: none;
+  }
+
+  :where(code):not(:where([class~="not-prose"], [class~="not-prose"] *))::after {
+    content: none;
+  }
+}
+</style>
