b46402d5abb3a32355c3137c561bd39c2f554320
mandy <mandy3152618@gmail.com>
2025-04-29 17:56:03 +0800

    feat: button ui 元件

---
+++ components/Base/Button.vue
@@ -0,0 +1,18 @@
+// customClass
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    disabled?: boolean;
+  }>(),
+  {
+    disabled: false,
+  }
+);
+
+</script>
+
+<template>
+  <button class="c-rounded-btn" :disabled="disabled">
+    <slot />
+  </button>
+</template>
