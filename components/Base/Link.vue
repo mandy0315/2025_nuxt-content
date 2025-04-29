434c58e03e2ed2e32b949e324695d2c9a1c01909
mandy <mandy3152618@gmail.com>
2025-04-29 17:55:39 +0800

    feat: link ui 元件

---
+++ components/Base/Link.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    to?: string;
+    href?: string;
+    isExternal?: boolean;
+  }>(),
+  {
+    to: undefined,
+    href: undefined,
+    isExternal: false,
+  }
+);
+
+</script>
+
+<template>
+  <NuxtLink v-if="to" :to class="c-rounded-btn">
+    <slot />
+  </NuxtLink>
+  <a v-else :href class="c-rounded-btn" :target="isExternal ? '_blank' : undefined"
+    :rel="isExternal ? 'noopener noreferrer' : undefined">
+    <slot />
+  </a>
+</template>
