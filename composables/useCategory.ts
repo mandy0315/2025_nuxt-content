ba767e42ca030e1fe810b357ba24346278b86344
mandy <mandy3152618@gmail.com>
2025-05-07 15:44:39 +0800

    fix: categories 命名

--- composables/useCategory.ts
+++ composables/useCategory.ts
@@ -7,7 +7,7 @@ export const useCategory = () => {
   };
 
   const getCategories = async (limit?: number) => {
-    const { data: categorys } = await useAsyncData("categories", async () => {
+    const { data: categories } = await useAsyncData("categories", async () => {
       const selectItemInPosts = await queryCollection("posts")
         .order("date", "DESC")
         .select("title", "categorys")
@@ -18,12 +18,11 @@ export const useCategory = () => {
       if (limit) {
         categorys = categorys.slice(0, limit);
       }
-      console.log(categorys);
       // 用 Set 去除重複的分類
       const uniqueCategorys = Array.from(new Set(categorys));
       return uniqueCategorys;
     });
-    return categorys;
+    return categories;
   };
 
   return { goToCategoryPage, getCategories };
