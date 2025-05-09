a42d7f6dd34ffeeb5e6877ec1fb330ec72d8dec3
mandy <mandy3152618@gmail.com>
2025-05-09 10:28:25 +0800

    feat: 新增全站搜尋元件並載入 header

--- components/App/Header.vue
+++ components/App/Header.vue
@@ -2,6 +2,7 @@
   <header
     class="bg-[var(--bg-color)]/80 backdrop-blur-sm h-c-header-height flex border-b c-border-gray w-full sticky top-0 justify-between z-50">
     <p>header</p>
+    <AppSearch />
     <AppColorModel />
   </header>
 </template>
---
+++ components/App/Search.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const keyword = '珍奶';
+
+const { updatedKeywords } = await useSearch();
+onMounted(async () => {
+  updatedKeywords(keyword);
+});
+
+const clearKeyword = async () => {
+  console.log('clearKeyword');
+  await updatedKeywords('');
+};
+
+
+</script>
+
+<template>
+
+  <div>search</div>
+  <button @click="clearKeyword">X</button>
+</template>
