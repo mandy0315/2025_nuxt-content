e6a47ea080fc08032dc218e5f1e80215ec15efd8
mandy <mandy3152618@gmail.com>
2025-05-11 01:06:39 +0800

    fix: keywords 與 updatedKeywords 移到 useSearch ; style: SearchModal 樣式

--- components/App/Search.vue
+++ components/App/Search.vue
@@ -1,21 +1,37 @@
 <script setup lang="ts">
-const keyword = '珍奶';
 
-const { updatedKeywords } = await useSearch();
-onMounted(async () => {
-  updatedKeywords(keyword);
+const { keywords, updatedKeywords, isShowSearchModal } = useSearch();
+watch(keywords, (newVal) => {
+  updatedKeywords(newVal);
+});
+onMounted(() => {
+  updatedKeywords('');
 });
 
-const clearKeyword = async () => {
-  console.log('clearKeyword');
-  await updatedKeywords('');
-};
 
 
 </script>
 
 <template>
 
-  <div>search</div>
-  <button @click="clearKeyword">X</button>
+  <button @click="isShowSearchModal = !isShowSearchModal">search</button>
+  <Teleport to="body">
+    <div v-if="isShowSearchModal">
+      <div class="w-5/10 fixed top-20 transform -translate-x-1/2 left-1/2 overflow-hidden z-110">
+        <button @click="updatedKeywords('')" class="absolute top-3 right-3 text-gray-500 z-20 text-2xl">
+          <Icon name="solar:close-circle-bold" />
+        </button>
+        <label class="relative w-full h-12 flex bg-white rounded-t border border-gray-300">
+          <input type="text" v-model.lazy="keywords" class=" px-10 w-full" placeholder="搜尋網站..." />
+          <Icon name="solar:magnifer-linear" class="absolute top-3.5 left-3 text-gray-500 z-10 text-lg" />
+        </label>
+      </div>
+
+      <div></div>
+
+      <div @click.stop="isShowSearchModal = false" class="fixed inset-0 w-full h-full bg-black/80 z-100"></div>
+    </div>
+  </Teleport>
+
+
 </template>
