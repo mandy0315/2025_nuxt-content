b8107e707c80273f989c3cb9b3f18732eaa4ec6c
mandy <mandy3152618@gmail.com>
2025-05-12 11:56:19 +0800

    fix: 更動搜尋與暗黑模式樣式

--- components/App/Header.vue
+++ components/App/Header.vue
@@ -2,7 +2,9 @@
   <header
     class="bg-[var(--bg-color)]/80 backdrop-blur-sm h-c-header-height flex border-b c-border-gray w-full sticky top-0 justify-between z-50">
     <p>header</p>
-    <AppSearch />
-    <AppColorModel />
+    <div class="flex items-center">
+      <AppSearch />
+      <AppColorModel />
+    </div>
   </header>
 </template>
