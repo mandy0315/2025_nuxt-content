15fc0b8ebc92bcab98a99b802bcc6cdcb46b0abf
mandy <mandy3152618@gmail.com>
2025-05-13 10:20:17 +0800

    fix: 刪除 pageMap 檔案

--- utils/pageMap.ts
+++
@@ -1,5 +0,0 @@
-export const pageMap = new Map<string, { path: string; title: string }>([
-  ["index", { path: "/", title: "首頁" }],
-  ["posts", { path: "/posts", title: "文章" }],
-  ["categories", { path: "/categories", title: "分類" }],
-]);
