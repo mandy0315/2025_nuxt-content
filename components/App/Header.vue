ab47462fc903e4a5d54afae744a9e838ceb7f5b8
mandy <mandy3152618@gmail.com>
2025-05-13 10:21:20 +0800

    style: header 新增 menu 與調整區塊其他樣式

--- components/App/Header.vue
+++ components/App/Header.vue
@@ -1,7 +1,22 @@
+<script setup lang="ts">
+import { pageMap } from "@/utils/mainMap";
+</script>
 <template>
   <header
-    class="bg-[var(--bg-color)]/80 backdrop-blur-sm h-c-header-height flex border-b c-border-gray w-full sticky top-0 justify-between z-50">
-    <p>header</p>
+    class="bg-[var(--bg-color)]/80 backdrop-blur-sm h-c-header-height flex border-b c-border-gray w-full sticky top-0 justify-between items-center z-50">
+    <NuxtLink to="/">
+      Logo
+    </NuxtLink>
+    <!-- menu -->
+    <ul class="flex items-center gap-4">
+      <li v-for="(item, index) in pageMap.values()" :key="index">
+        <NuxtLink v-if="item.title !== '首頁'" :to="item.path"
+          class="text-[var(--text-color)] hover:text-[var(--primary-color)] transition-all duration-300 ease-in-out">
+          {{ item.title }}
+        </NuxtLink>
+      </li>
+    </ul>
+    <!-- tools -->
     <div class="flex items-center">
       <AppSearch />
       <AppColorModel />
