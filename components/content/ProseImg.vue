2d96362b803554e61da3efb3434d66bcae847b79
mandy <mandy3152618@gmail.com>
2025-05-13 11:41:56 +0800

    feat: 自訂 ProseImg

---
+++ components/content/ProseImg.vue
@@ -0,0 +1,25 @@
+<template>
+  <NuxtImg :src :alt :width :height />
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+  src: {
+    type: String,
+    default: ''
+  },
+  alt: {
+    type: String,
+    default: ''
+  },
+  width: {
+    type: [String, Number],
+    default: undefined
+  },
+  height: {
+    type: [String, Number],
+    default: undefined
+  }
+})
+</script>
